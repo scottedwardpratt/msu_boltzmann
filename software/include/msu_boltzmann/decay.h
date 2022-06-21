a5395482c1cba5c56e754ac45ab01293130de4af
scottedwardpratt <prattsc@msu.edu>
2022-06-21 18:11:34 -0400

    dummy message

---
+++ software/include/msu_boltzmann/decay.h
@@ -0,0 +1,22 @@
+#ifndef __MSU_DECAY_H__
+#define __MSU_DECAY_H__
+
+#include "msu_boltzmann.h"
+
+using namespace std;
+
+class CMSU_Decay{
+public:
+	vector<double> mass,probmax;
+	vector<FourVector> p;
+	FourVector u,pprime;
+	char message[200];
+	CDecay_NBody *decay_nbody;
+	Crandy *randy;
+	CMSU_Decay(Crandy *randy);
+	void GetMassesForDecay(vector<double> &mass,int nbodies,array<CMSUPart *,5> &daughter);
+	void Decay(CMSUPart *mother,int &nbodies,array<CMSUPart *,5> &daughter);
+	CMSU_Boltzmann *boltzmann;
+};
+
+#endif
